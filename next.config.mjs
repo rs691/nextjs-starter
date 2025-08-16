2441919b199adbbf5d4a85cb85fc08df734a1c00
rs691 <robstewart@my365.bellevue.edu>
2025-08-16 18:22:45 -0500

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: '/nextjs-starter', // Repo name
+    assetPrefix: '/nextjs-starter/', // Repo name
   sassOptions: {
     compiler: "modern",
     silenceDeprecations: ["legacy-js-api"],
