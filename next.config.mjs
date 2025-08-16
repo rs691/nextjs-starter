dacd70bba3d8dd2306b08fb78d1886f0a539f092
rs691 <robstewart@my365.bellevue.edu>
2025-08-16 18:28:26 -0500

    updated config 2

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
     basePath: '/nextjs-starter', // Repo name
     assetPrefix: '/nextjs-starter/', // Repo name
+      images: {
+    unoptimized: true // Disable image optimization
+  },
   sassOptions: {
     compiler: "modern",
     silenceDeprecations: ["legacy-js-api"],
